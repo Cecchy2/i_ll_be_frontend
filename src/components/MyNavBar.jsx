db604ec13c284c4e59fd3033b86b753194f007e9
Cecchy2 <dariocecchinato@gmail.com>
2024-11-06 13:48:15 +0100

    spostato dropdown

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -17,19 +17,20 @@ const MyNavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
         <Nav className="ms-auto">
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
           <Form className="d-flex">
             <Form.Control type="search" placeholder="Cerca per nome o email" className="me-2" aria-label="Search" />
             <Button variant="outline-info" type="submit">
               Cerca
             </Button>
           </Form>
+
+          <NavDropdown title="Login" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
         </Nav>
       </Container>
     </Navbar>
