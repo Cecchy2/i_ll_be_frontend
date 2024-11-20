dba5c341972e6e97ef2f7cc7e8e0a40a2cc4e04a
Cecchy2 <dariocecchinato@gmail.com>
2024-11-20 23:07:59 +0100

    aggiunte card amici su amiciPage

--- src/components/AmiciPage.jsx
+++ src/components/AmiciPage.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getAmici } from "../redux/actions/amiciActions";
+import { useNavigate } from "react-router-dom";
 
 const AmiciPage = () => {
   const utente = useSelector((state) => state.utente.utente);
   const dispatch = useDispatch();
   const amici = useSelector((state) => state.amicizie.amici);
+  const navigate = useNavigate();
   console.log(utente);
   console.log(amici);
 
@@ -22,7 +24,18 @@ const AmiciPage = () => {
     <div className="main">
       <Container>
         <Row>
-          <Col></Col>
+          {amici.map((amico) => (
+            <Col key={amico.id}>
+              <Card className="m-3 p-3 amiciCard text-white " bg="dark">
+                <Card.Img variant="top" src={amico.immagine} className="amiciImage border border-warning" />
+                <Card.Body>
+                  <Card.Title className="m-0 fs-6">{amico.username}</Card.Title>
+                  <Card.Title className="m-0 fs-6">{amico.nome}</Card.Title>
+                  <Card.Title className="m-0 fs-6">{amico.cognome}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>{" "}
     </div>
