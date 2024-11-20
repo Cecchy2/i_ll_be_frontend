3b00f21f24777dfda203a824016d8fc23e3f3723
Cecchy2 <dariocecchinato@gmail.com>
2024-11-20 15:51:24 +0100

    aggiunte cards utenti

--- src/components/AmiciPage.jsx
+++ src/components/AmiciPage.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import { getAllUtenti } from "../redux/actions/amiciActions";
 import { useDispatch, useSelector } from "react-redux";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 
 const AmiciPage = () => {
   const dispatch = useDispatch();
-  const utenti = useSelector((state) => state.amicizie.utenti);
+  const utenti = useSelector((state) => state.amicizie.utenti.content);
   console.log(utenti);
 
   useEffect(() => {
@@ -12,7 +13,22 @@ const AmiciPage = () => {
   }, [dispatch]);
   return (
     <div className="main">
-      <h1>AmiciPage</h1>
+      <Container>
+        <Row>
+          <Col className="d-flex">
+            {utenti?.map((utente) => (
+              <Card key={utente.id} className="m-3 p-3 amiciCard">
+                <Card.Img variant="top" src={utente.immagine} className="amiciImage " />
+                <Card.Body>
+                  <Card.Title className="m-0 fs-6">{utente.username}</Card.Title>
+                  <Card.Title className="m-0 fs-6">{utente.nome}</Card.Title>
+                  <Card.Title className="m-0 fs-6">{utente.cognome}</Card.Title>
+                </Card.Body>
+              </Card>
+            ))}
+          </Col>
+        </Row>
+      </Container>{" "}
     </div>
   );
 };
